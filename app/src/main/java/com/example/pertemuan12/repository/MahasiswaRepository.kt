22d243936af01b4c006358913dad22564db4ec95
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-30 08:33:27 +0700

    mengubah datapada MahasiswaRepository.kt

--- app/src/main/java/com/example/pertemuan12/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/pertemuan12/repository/MahasiswaRepository.kt
@@ -1,16 +1,50 @@
 package com.example.pertemuan12.repository
 
 import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.service.MahasiswaService
+import okio.IOException
 
-interface MahasiswaRepository {
+interface MahasiswaRepository{
+    suspend fun getMahasiswa(): List<Mahasiswa>
 
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 
-    suspend fun getMahasiswa(): List<Mahasiswa>
-
-    suspend fun  updateMahasiswa (nim: String, mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
 
     suspend fun deleteMahasiswa(nim: String)
 
-    suspend fun  getMahasiswaByNim(nim: String): Mahasiswa
+    suspend fun getMahasiswaById(nim: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP Status code: " +
+                        "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        return mahasiswaService.getMahasiswaById(nim)
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaService.getMahasiswa()
 }
