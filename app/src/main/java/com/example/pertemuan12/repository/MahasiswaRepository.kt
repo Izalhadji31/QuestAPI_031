d94deaea790d673c7e62b5b27bf67a566534cd48
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-30 08:24:28 +0700

    menambahkan interface MahasiswaRepository

---
+++ app/src/main/java/com/example/pertemuan12/repository/MahasiswaRepository.kt
@@ -0,0 +1,16 @@
+package com.example.pertemuan12.repository
+
+import com.example.pertemuan12.model.Mahasiswa
+
+interface MahasiswaRepository {
+
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    suspend fun  updateMahasiswa (nim: String, mahasiswa: Mahasiswa)
+
+    suspend fun deleteMahasiswa(nim: String)
+
+    suspend fun  getMahasiswaByNim(nim: String): Mahasiswa
+}
