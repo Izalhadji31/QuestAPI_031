4e09dba155babf528ee71e23645447f5eab0f753
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-30 09:31:55 +0700

    menambahkan data pada strings.xml

--- app/src/main/java/com/example/pertemuan12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/HomeView.kt
@@ -34,7 +34,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.R
